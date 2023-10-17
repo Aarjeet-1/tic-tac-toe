8562a4a53cd49df2e80f410eaa2094a9beb61005
Aarjeet-1 <Encrypti>
2023-10-17 09:46:57 +0545

    version 1

---
+++ game.py
@@ -0,0 +1,54 @@
+from new import board_draw, check_turn,check_for_win
+
+import os
+#making a key
+
+spots = {1 : "1", 2 : "2", 3 : "3" , 4 : "4" , 5 : "5" ,
+        6 : "6" , 7 : "7" , 8 : "8" , 9 : "9" }
+
+playing = True
+complete = False
+turn = 0
+prev_turn = -1
+
+#game loop
+while playing:
+    #reset the screen
+    os.system('cls' if os.name =="nt"else 'clear')
+    board_draw(spots)
+
+    if prev_turn == turn:
+        print("Invalid spot selected,please pick another.")
+        prev_turn = turn
+    print("Player" + str((turn%2)+1)+ "'s turn : Pick your spot or press q to quit")
+
+    #input from user
+    choice = input()
+    # the game has ended,
+    if choice == 'q':
+        playing = False
+        #check if player gave a number from 1-9
+    elif str.isdigit(choice) and int(choice)in spots:
+        #check if the spot has already been taken
+        if not spots[int(choice)] in {"X", "O"}:
+            # valid input, update the board
+            
+            turn +=1
+            spots[int(choice)] = check_turn(turn) 
+
+
+    #check if the game has won by any player
+    
+    if check_for_win(spots):playing,complete = False , True   
+
+    if turn >8 : playing = False
+    if complete:
+        if check_turn(turn) =="X :" print("Player 1 Wins!")
+    else:
+        print("PLayer 2 Wins!")
+
+    else:
+        print("No Winner")
+
+
+
